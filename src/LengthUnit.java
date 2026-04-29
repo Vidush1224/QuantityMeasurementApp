8d126048328de5e9b13ccdb60c383471c897f800
Vidush1224 <vidush.skv@gmail.com>
2026-04-29 09:00:09 +0530

    UC10: Generic Quantity Class with Unit Interface for Multi-Category Support

--- src/LengthUnit.java
+++ src/LengthUnit.java
@@ -1,13 +1,12 @@
-public enum WeightUnit implements IMeasurable {
-    MILLIGRAM(0.001),
-    GRAM(1.0),
-    KILOGRAM(1000.0),
-    POUND(453.592),
-    TONNE(1_000_000.0);
+public enum LengthUnit implements IMeasurable {
+    FEET(12.0),
+    INCHES(1.0),
+    YARDS(36.0),
+    CENTIMETERS(0.393701);
 
     private final double factor;
 
-    WeightUnit(double factor) {
+    LengthUnit(double factor) {
         this.factor = factor;
     }
 
